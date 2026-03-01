697727f3f8f9680a3faf9853b6286659d9347e3d
narvarzkevin8-png <narvarzkevin8@gmail.com>
2026-03-01 16:54:43 -0400

    Add files via upload

---
+++ Commands/horarios.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('horario')
+        .setDescription('Crea un mensaje de partido')
+        .addStringOption(option =>
+            option.setName('equipo_rival')
+                .setDescription('Nombre del equipo rival')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('liga')
+                .setDescription('Nombre de la liga')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('hora')
+                .setDescription('Hora del partido')
+                .setRequired(true)),
+
+    async execute(interaction) {
+
+        const rival = interaction.options.getString('equipo_rival');
+        const liga = interaction.options.getString('liga');
+        const hora = interaction.options.getString('hora');
+
+        const mensaje =
+`# **__PARTIDO__**
+
+- *🔱EQUIPO RIVAL🔱*: \`${rival}\`
+- *🪩LIGA🪩*: \`${liga}\`
+- *🏟️HORA🏟️*: \`${hora}\``;
+
+        await interaction.reply({ content: mensaje });
+    }
+};
